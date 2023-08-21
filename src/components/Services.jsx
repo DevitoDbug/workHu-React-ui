43db834d552bd7f943f3b9793592b1168d2d6c36
DevitoDbug <Davidochiengy@gmail.com>
2023-08-21 20:28:52 +0300

    Fixed a URL bug

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import Service from "./Service";
 
-//<a href="https://www.flaticon.com/free-icons/restaurant" title="restaurant icons">Restaurant icons created by Eucalyp - Flaticon</a>
-//<a href="https://www.flaticon.com/free-icons/swimming" title="swimming icons">Swimming icons created by Chattapat - Flaticon</a>
-//<a href="https://www.flaticon.com/free-icons/pay" title="pay icons">Pay icons created by amonrat rungreangfangsai - Flaticon</a>
-//<a href="https://www.flaticon.com/free-icons/credit-card" title="credit card icons">Credit card icons created by Vectors Market - Flaticon</a>
-//<a href="https://www.flaticon.com/free-icons/assistance" title="assistance icons">Assistance icons created by Flat Icons - Flaticon</a>
 const Services = () => {
   const services = [
     {
       id: 1,
-      logoURL: "../../public/online-payment.png",
+      logoURL: "/../../public/online-payment.png",
       description: "Lorem, ipsum dolor sit amet consectetur elit.",
     },
     {
       id: 2,
-      logoURL: "../../public/swimming.png",
+      logoURL: "/../../public/swimming.png",
       description: "Lorem, ipsum dolor sit amet consectetur elit.",
     },
     {
@@ -25,7 +20,7 @@ const Services = () => {
     },
     {
       id: 4,
-      logoURL: "../../public/online-payment.png",
+      logoURL: "/../../public/online-payment.png",
       description: "Lorem, ipsum dolor sit amet consectetur elit.",
     },
     {
@@ -35,7 +30,7 @@ const Services = () => {
     },
     {
       id: 6,
-      logoURL: "../../public/restaurant.png",
+      logoURL: "/../../public/restaurant.png",
       description: "Lorem, ipsum dolor sit amet consectetur elit.",
     },
   ];
