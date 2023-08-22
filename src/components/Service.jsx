e2bddd3389b5b447e4ea3ab6b198fcb0173fa28c
DevitoDbug <Davidochiengy@gmail.com>
2023-08-22 08:35:20 +0300

    Modified services

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Service = ({ service }) => {
   return (
-    <div className="bg-C_White flex flex-col text-center p-2 justify-center items-center w-40 h-44 shadow-md rounded-lg hover:shadow-sm hover:shadow-C_GreenSlit hover:scale-[1.05] transition duration-[0.12s] ease-in">
+    <div className="bg-C_White flex flex-col text-center p-2 justify-center items-center w-36 h-48 shadow-md rounded-lg hover:shadow-sm hover:shadow-C_GreenSlit hover:scale-[1.05] transition duration-[0.12s] ease-in">
       <img src={service.logoURL} alt="logo" className="w-12 h-12" />
       <h2 className="text-C_TextBlack font-bold p-2">{service.name}</h2>
       <p className="text-sm text-C_UserDullBlack p-2">{service.description}</p>
