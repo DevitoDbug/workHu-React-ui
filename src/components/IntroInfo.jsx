5c29c6ad73a7f0c28591180a10416e0e3b373c7b
DevitoDbug <Davidochiengy@gmail.com>
2023-08-22 08:21:52 +0300

    Fixed an image URL

--- src/components/IntroInfo.jsx
+++ src/components/IntroInfo.jsx
@@ -1,13 +1,10 @@
 import React from "react";
+import image1 from "../../public/thinking.svg";
 
 const IntroInfo = () => {
   return (
     <div className="bg-C_DullWhite h-2/3 flex flex-col gap-2 items-center justify-center">
-      <img
-        className="mt-6 w-[70%]"
-        src="/../../public/thinking.svg"
-        alt="focusing"
-      />
+      <img className="mt-6 w-[70%]" src={image1} alt="focusing" />
       <div className="p-3">
         <h2 className="text-gray-800 text-2xl font-bold mt-4 leading-tight">
           Your customixed Token For Globally Complient Distribution Events
