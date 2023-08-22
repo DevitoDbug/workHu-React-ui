bec985deaa33e2e9da6888c80caad53610c604ee
DevitoDbug <Davidochiengy@gmail.com>
2023-08-22 11:00:05 +0300

    Few asthetic changes to ui

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -4,8 +4,8 @@ const Service = ({ service }) => {
   return (
     <div className="bg-C_White flex flex-col text-center p-2 justify-center items-center w-36 h-48 shadow-md rounded-lg hover:shadow-sm hover:shadow-C_GreenSlit hover:scale-[1.05] transition duration-[0.12s] ease-in">
       <img src={service.logoURL} alt="logo" className="w-12 h-12" />
-      <h2 className="text-C_TextBlack font-bold p-2">{service.name}</h2>
-      <p className="text-sm text-C_UserDullBlack p-2">{service.description}</p>
+      <h2 className="text-C_TextBlack font-bold px-2">{service.name}</h2>
+      <p className="text-sm text-gray-700 p-2">{service.description}</p>
     </div>
   );
 };
